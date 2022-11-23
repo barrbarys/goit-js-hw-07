308855e0552ab857fc311df82ec58aa159ea35a6
barrbarys <pysarchuk.ihor@gmail.com>
2022-11-23 11:23:12 +0200

    Before mentor

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryBox = document.querySelector(".gallery");
+const galleryElements = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+);
+galleryBox.insertAdjacentHTML("beforeend", galleryElements.join(""));
+galleryBox.addEventListener("click", onImageClick);
+function onImageClick(evt) {
+  evt.preventDefault();
+  if (evt.target.className !== "gallery_image") {
+    return;
+  }
+  modalImageControl(evt.target.dataset.source);
+}
+let galleryStatus;
+function modalImageControl(link) {
+  galleryStatus = basicLightBox.create(
+    `
+    <img src="${link}" width="800" height="600">
+    `,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", onListenerKeyboard);
+      },
+      onClose: () => {
+        document.addEventListener("keydown", onListenerKeyboard);
+      },
+    }
+  );
+  galleryStatus.show();
+}
+function onlistenerKeyboard(evt) {
+  if (evt.code === "Escape") {
+    return galleryStatus.close();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+//**************************************************** */
+
+// Create gallery of images/photos
+
+const galleryBox = document.querySelector(".gallery");
+
+const galleryElements = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<li class="gallery__item"><a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}"
+    alt="${description}" /></a></li>`
+);
+
+galleryBox.insertAdjacentHTML("beforeend", galleryElements.join(""));
+
+// Added the functional from SimpleLightbox's library
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
+
+const bodyRef = document.querySelector("body");
+
+gallery.on("shown.simplelightbox", function () {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+});
+
+gallery.on("changed.simplelightbox", function () {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+});
+
+gallery.on("close.simplelightbox", function () {
+  bodyRef.style.backgroundColor = "#ffffff";
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
